8564fdd3471bc7a5dad56bf3df4eb81d4daecfaa
DarkKittyInTheSack <vinhnguyen1906dev@gmail.com>
2024-01-02 16:11:07 +0700

    UUpdate

---
+++ src/pages/HomePage/FilmList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const FilmList = () => {
+
+  return (
+    <div className='container my-3'>
+        <div className="flex">
+            <button className='bg-blue-800 text-white px-2 py-1'>Phim đang chiếu</button>
+            <button>Phim sắp chiếu</button>
+        </div>
+        
+    </div>
+  )
+}
+
+export default FilmList
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Banner from "./Banner";
 import LichChieuRap from "./LichChieuRap";
+import FilmList from "./FilmList";
 
 const HomePage = () => {
   return (
     <div>
       <Banner />
+      <FilmList/>
       <LichChieuRap />
     </div>
   );
