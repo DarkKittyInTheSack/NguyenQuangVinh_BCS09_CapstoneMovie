71276546301af6b2e3cd1bce0093bf670e099003
DarkKittyInTheSack <vinhnguyen1906dev@gmail.com>
2024-01-02 16:23:32 +0700

    Update

--- src/pages/HomePage/FilmList.js
+++ src/pages/HomePage/FilmList.js
@@ -5,10 +5,14 @@ const FilmList = () => {
   return (
     <div className='container my-3'>
         <div className="flex">
-            <button className='bg-blue-800 text-white px-2 py-1'>Phim đang chiếu</button>
-            <button>Phim sắp chiếu</button>
+            <button className='bg-blue-800 text-white px-2 py-1 rounded mr-3'>Phim đang chiếu</button>
+            <button className='bg-blue-800 text-white px-2 py-1 rounded'>Phim sắp chiếu</button>
         </div>
         
+        <div>
+          
+        </div>
+
     </div>
   )
 }
