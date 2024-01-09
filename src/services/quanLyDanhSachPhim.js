b9b8fa6918a1f4e8753f5cb4dcac570bb491e303
DarkKittyInTheSack <vinhnguyen1906dev@gmail.com>
2024-01-09 12:18:32 +0700

    update

--- src/services/quanLyDanhSachPhim.js
+++ src/services/quanLyDanhSachPhim.js
@@ -2,7 +2,7 @@ import { https } from './configServ';
 
 export const quanLyDanhSachPhim = {
     getMovieByName: (name) => {
-        return https.get(`/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP08&tenPhim=${name}`)
+        return https.get(`/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP02&tenPhim=${name}`)
     },
 
     getMovieByID: (id) =>{
